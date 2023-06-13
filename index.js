c93957e8ace76ee01bcb1d7f0cafa51d2c28ff7c
Pradeep2888 <pradeeptiwari2888@gmail.com>
2023-06-13 17:55:58 +0530

    craeted two secound routes for contact us form

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get("/",(req,res)=>{
     res.send("welcome to api")
 })
 
-app.post("/generatemail",(req,res)=>{
+app.post("/enquiry",(req,res)=>{
     const {name,email,contact,message}=req.body
     let config = {
 
@@ -44,7 +44,7 @@ app.post("/generatemail",(req,res)=>{
     let mailgenerate1 = {
         from : "acctingszone@gmail.com",
         to : email,
-        subject: "Thanking Mail From estudee ",
+        subject: "Thanking Mail From Acctings Zone ",
         html: mail1
     }
     
@@ -94,6 +94,106 @@ app.post("/generatemail",(req,res)=>{
 
 
 })
+
+
+
+
+
+
+
+
+
+
+
+app.post("/contactus",(req,res)=>{
+    const {name,email,contact,message,subject}=req.body
+    let config = {
+
+        service : 'gmail',
+        auth : {
+            user:"acctingszone@gmail.com",
+            pass: "jbovyygqzhnxpsgx"
+        }
+    }
+
+    let transporter = nodemailer.createTransport(config);
+
+    let MailGenerator = new Mailgen({
+        theme: "default",
+        product : {
+            name: "Accountings Zone",
+            link : 'https://accountingszone.com/'
+        }
+    })
+    //  ******Thanking Mail Sending to visitor****
+    let response1 = {
+        body: {
+            name : name,
+            intro: "Thank you for taking the time to visit our website we will connect to you shortly",
+        }
+    }
+
+    let mail1 = MailGenerator.generate(response1)
+
+    let mailgenerate1 = {
+        from : "acctingszone@gmail.com",
+        to : email,
+        subject: "Thanking Mail From Acctings Zone ",
+        html: mail1
+    }
+    
+    transporter.sendMail(mailgenerate1).then(() => {
+        return res.status(201).json({
+            msg: "you should receive an email"
+        })
+    }).catch(error => {
+        console.log(mailgenerate1)
+        return res.status(500).json({  msg: "something went wrong" })
+    })
+
+
+
+    // *****Vistor information mail Sending to Company*******
+
+
+
+    let response2 = {
+        body: {
+            name : "Accountings Zone",
+            intro: `Name:${name} Contact:${contact} Email:${email} Message:${message}`,
+        }
+    }
+
+    let mail2 = MailGenerator.generate(response2)
+
+    let mailgenerate2 = {
+        from : "acctingszone@gmail.com",
+        to : "acctingszone@gmail.com",
+        subject: subject,
+        html: mail2
+    }
+
+    
+    transporter.sendMail(mailgenerate2).then(() => {
+       console.log("mail to company sended")
+    
+    }).catch(error => {
+        console.log(mailgenerate2)
+        console.log("something went wrong")
+       
+    })
+
+
+
+
+
+})
+
+
+
+
+
+
 
 
 
